72879337ddef930453eadab399f9b7c4f3bc98ba
EgorZicov <zicbor@mail.ru>
2023-11-07 12:42:40 +0300

    levels

--- Batty.py
+++ Batty.py
@@ -1,15 +1,23 @@
 import pygame
 
-
 def create_blocks():
+    global block_img
     blocks = []
-    y_block = 75
-    for j in range(3):
-        x_block = 225
-        for i in range(5):
+    space_between_blocks = 25
+    rows = 10 + level
+    while y_platform - 3 * space_between_blocks < rows * (block_img.get_height() + space_between_blocks):
+        block_img = pygame.transform.scale(block_img, (block_img.get_width() * 0.9, block_img.get_height() * 0.9))
+        
+    colums = (SCREEN_WIDTH - space_between_blocks) // (block_img.get_width() + space_between_blocks)
+    border_space = (SCREEN_WIDTH - (colums * (block_img.get_width() + space_between_blocks) - space_between_blocks)) // 2
+    
+    y_block = border_space
+    for j in range(rows):
+        x_block = border_space
+        for i in range(colums):
             blocks.append((x_block, y_block))
-            x_block += 125
-        y_block += 75
+            x_block += (block_img.get_width() + space_between_blocks)
+        y_block += (block_img.get_height() + space_between_blocks)
     return blocks
 
 
@@ -17,28 +25,28 @@ def show_blocks():
     for x, y in blocks:
         screen.blit(block_img, block_img.get_rect(topleft=(x, y)))
 
-def collision(LEFT, TOP, RIGHT, BOTTOM):
+def collision(LEFT, TOP, RIGHT, BOTTOM): # -1 -- левая, верхняя
     d = dist(LEFT, TOP, x_circle, y_circle)
     if d <= radius_circle:
-        return 1, 1    
+        return -1, -1    
     d = dist(RIGHT, TOP, x_circle, y_circle)
     if d <= radius_circle:    
-        return 1, 1
+        return 1, -1
     d = dist(LEFT, BOTTOM, x_circle, y_circle)
     if d <= radius_circle: 
-        return 1, 1
+        return -1, 1
     d = dist(RIGHT, BOTTOM, x_circle, y_circle)
     if d <= radius_circle: 
         return 1, 1
     # левая стенка
     if 0 < LEFT - x_circle <= radius_circle and TOP <= y_circle <= BOTTOM:
-        return 1, 0
+        return -1, 0
     # правая стенка
     if 0 < x_circle - RIGHT <= radius_circle and TOP <= y_circle <= BOTTOM:
         return 1, 0
     # верхняя стенка
     if 0 < TOP - y_circle <= radius_circle and LEFT <= x_circle <= RIGHT:
-        return 0, 1
+        return 0, -1
     # нижняя стенка
     if 0 < y_circle - BOTTOM <= radius_circle and LEFT <= x_circle <= RIGHT:
         return 0, 1
@@ -50,7 +58,7 @@ def dist(X1, Y1, X2, Y2):
     c = (x**2 + y**2)**0.5
     return c
 
-FPS = 120
+FPS = 120 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -64,26 +72,30 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
+level = 1
+
 platform_img = pygame.image.load('images\\platform.png').convert()
 platform_img = pygame.transform.scale(platform_img, (200, 40))
 
 block_img = pygame.image.load('images\\block.png').convert()
 block_img = pygame.transform.scale(block_img, (100, 50))
-blocks = create_blocks()
 radius_circle = 15
 
-x_platform = SCREEN_WIDTH // 2
-y_platform = SCREEN_HEIGHT // 4 * 3
+
 
 speed_platform = 0
 
 
 def set_start():
-    global x_circle, y_circle, v_x, v_y
-    x_circle = 200
-    y_circle = 500
+    global x_circle, y_circle, v_x, v_y, x_platform, y_platform, blocks, freeze_flag
+    x_circle = SCREEN_WIDTH // 2
+    y_circle = SCREEN_HEIGHT // 4 * 3 -20
+    x_platform = SCREEN_WIDTH // 2
+    y_platform = SCREEN_HEIGHT // 4 * 3
     v_x = -3
     v_y = -3
+    blocks = create_blocks()
+    freeze_flag = False
 
 
 set_start()
@@ -100,7 +112,6 @@ while True:
                 speed_platform += 6
             if event.key == pygame.K_TAB:
                 set_start()
-                blocks = create_blocks()
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 speed_platform += 6
@@ -122,7 +133,7 @@ while True:
         v_y = -v_y
     if y_circle > SCREEN_HEIGHT - radius_circle:
         y_circle = SCREEN_HEIGHT - radius_circle
-        x_circle = SCREEN_WIDTH // 2
+        # x_circle = SCREEN_WIDTH // 2
         v_y = 0
         v_x = 0
     
@@ -135,35 +146,63 @@ while True:
 
     # шарик и платформа (столкновение)
     # TODO Дописать условие сталкновения с платформой (бока + не проваливаться + разные углы)
-    higher_than_platform = x_platform - platform_img.get_width() // 2 <= x_circle <= x_platform + platform_img.get_width() // 2
-    if 0 <= y_platform - platform_img.get_height() - y_circle <= radius_circle and higher_than_platform:
-        v_y = - v_y
-        y_circle += v_y
-    # qx, qy = collision(
-    #         x_platform - platform_img.get_width() // 2, 
-    #         y_platform - platform_img.get_height() // 2, 
-    #         x_platform + platform_img.get_width() // 2, 
-    #         y_platform + platform_img.get_height() // 2
-    #     )
-    # if qx == 1:
-    #     v_x = -v_x        
-    # if qy == 1:
-    #     v_y = -v_y
+    # higher_than_platform = x_platform - platform_img.get_width() // 2 <= x_circle <= x_platform + platform_img.get_width() // 2
+    # if 0 <= y_platform - y_circle <= radius_circle and higher_than_platform:
+    #     v_y = - v_y
+    #     y_circle += v_y
+    qx, qy = collision(
+            x_platform - platform_img.get_width() // 2, 
+            y_platform - platform_img.get_height() // 2, 
+            x_platform + platform_img.get_width() // 2, 
+            y_platform + platform_img.get_height() // 2
+        )
+    # -1 -- левая, верхняя
+
+    rebound_coefficient = 1
+    if qx == 1 and v_x < 0:
+        v_x = -v_x  
+        x_circle += v_x * rebound_coefficient
+    elif qx == 1 and v_x > 0:
+        x_circle += v_x * rebound_coefficient
+    elif qx == -1 and v_x > 0:
+        v_x = -v_x
+        x_circle += v_x * rebound_coefficient
+    elif qx == -1 and v_x < 0:
+        x_circle += v_x * rebound_coefficient
+
+    if qy == 1 and v_y < 0:
+        v_y = -v_y  
+        y_circle += v_y * rebound_coefficient
+    elif qy == 1 and v_y > 0:
+        y_circle += v_y * rebound_coefficient
+    elif qy == -1 and v_y > 0:
+        v_y = -v_y
+        y_circle += v_y * rebound_coefficient
+    elif qy == -1 and v_y < 0:
+        y_circle += v_y * rebound_coefficient
+
     # шарик и блоки (столкновение)
     for x_block, y_block in blocks:
         qx, qy = collision(x_block, y_block, x_block + block_img.get_width(), y_block + block_img.get_height())
         if qx == qy == 0:
             continue
-        if qx == 1:
+        if qx != 0:
             v_x = -v_x
-        if qy == 1:
+        if qy != 0:
             v_y = -v_y
         blocks.remove((x_block, y_block))
         break
-
+    if not blocks:
+        level += 1
+        set_start()
+        freeze_flag = True
     screen.fill(BLACK)
     screen.blit(platform_img, platform_img.get_rect(center=(x_platform, y_platform)))
     
     show_blocks()
     pygame.draw.circle(screen, RED, (x_circle,y_circle), radius = radius_circle)
     pygame.display.update()
+
+    if freeze_flag:
+        pygame.time.delay(3000)
+        freeze_flag = False
